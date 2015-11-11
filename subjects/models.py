6101b8274e60dd1603bb76aa390028406f9e9afa
nasuzi <nsuzi@icloud.com>
2015-11-11 18:14:04 +0600

    Changes to package "subjects" - Made description optional.

--- subjects/models.py
+++ subjects/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Subject(models.Model):
     name = models.CharField(max_length=15)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
