45b4dd1ee8a473fd49669a207afcbb9473a6e117
Suzi <nsuzi@icloud.com>
2015-11-25 17:35:16 +0600

    Updated subjects view and created subject_detail.html

--- subjects/models.py
+++ subjects/models.py
@@ -9,5 +9,6 @@ class Subject(models.Model):
     course = models.ForeignKey(Course)
     name = models.CharField(max_length=25)
     description = models.TextField(blank=True)
+
     def __str__(self):
         return self.name
--- subjects/views.py
+++ subjects/views.py
@@ -12,30 +12,3 @@ class SubjectList(ListView):
 class SubjectDetail(DetailView):
     model = Subject
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        for student in self.object.student_set.all():
-            students = []
-            students = student.name_set.order_by('subject')
-        for subject in self.object.subject_set.all():
-            subjects = []
-            subjects = subject.name_set.order_by('course')
-        for research in self.object.research_set.all():
-            researches = []
-            researches = research.subject_set.order_by('date')
-        for assignment in self.object.assignment_set.all():
-            assignments = []
-            assignments = assignment.subject_set.order_by('date')
-            subjects.append(dict(
-                pk=subject.pk,
-                name=research.name,
-                rdate=research.date,
-                assignment=assignment.name,
-                students=students,
-            ))
-        context['subjects'] = subjects
-        print(subjects)
-        print(students)
-        print(researches)
-        print(assignments)
-        return context
